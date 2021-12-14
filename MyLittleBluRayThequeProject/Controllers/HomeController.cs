0b56d52706bd5530922b6f9fea400f3826d8836e
Manon JULIEN <manon.julien@reseau.eseo.fr>
2021-12-14 16:22:16 +0100

    Revert "Revert "[BluRayRepository] commentaires, requête SQL, InsertPersonne""
    This reverts commit 1b7ef4cea3d20feb66cd6dde5d97d081d22ee01e.

--- MyLittleBluRayThequeProject/Controllers/HomeController.cs
+++ MyLittleBluRayThequeProject/Controllers/HomeController.cs
@@ -43,6 +43,10 @@ namespace MyLittleBluRayThequeProject.Controllers
         {
             return View();
         }
+        public IActionResult InsertPersonne()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
