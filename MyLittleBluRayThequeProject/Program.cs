6e2fa8f6a16d272c5a37e05ca9064e27ff945e14
Timothee <timothee.jouffrieau@gmail.com>
2022-01-04 10:13:41 +0100

    Add API

--- MyLittleBluRayThequeProject/Program.cs
+++ MyLittleBluRayThequeProject/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -27,5 +35,7 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name : "BluRayDisplay",
     pattern: "{controller=Home}/{action=SelectedBluRay}/{id?}");
-    
+
+app.MapControllers();
+
 app.Run();
